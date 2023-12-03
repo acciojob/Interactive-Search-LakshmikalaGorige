ec02e74299619022ae4386a039acc106c29e88fe
LakshmikalaGorige <124250141+LakshmikalaGorige@users.noreply.github.com>
2023-12-03 19:08:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+
+function activateSearch() {
+    var searchComponent = document.getElementById("searchComponent");
+    searchComponent.classList.add("active");
+
+    var searchInput = document.getElementById("searchInput");
+    searchInput.focus();
+}
